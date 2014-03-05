65d5013bf22d8c5806cdc47d50dfd8123566428b
Lukas Eipert <git@valedictorum.de>
2014-03-05 16:15:30 +0100

    Added bootstrap components.

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'ui.bootstrap',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
