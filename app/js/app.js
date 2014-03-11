b95304f628677bf2a205d1ea77cb6dbb060ea0f9
Lukas Eipert <git@valedictorum.de>
2014-03-11 10:16:26 +0100

    Added first functionalities. Renamed "MyApp" to GSB. Moved business logic to appropriate JS files.

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,13 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('GSB', [
   'ngRoute',
   'ui.bootstrap',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'GSB.filters',
+  'GSB.services',
+  'GSB.directives',
+  'GSB.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,27 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('GSB.controllers', []).
+  controller('SubjectCtrl', ['$scope', function($scope) {
+
+        $scope.properties = []
+
+        $scope.available = [
+            {text:'areaTotal'},
+            {text:'populationTotal'}
+        ];
+
+
+        $scope.addProperty = function() {
+            console.log($scope.propertySelected)
+            $scope.properties.push($scope.propertySelected);
+            $scope.propertySelected = '';
+        };
+        $scope.removeProperty = function() {
+            alert($scope.properties.text)//$scope.properties.slice()
+        };
 
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('PropertyCtrl', ['$scope', function($scope) {
+        $scope.test = 'Test';
   }]);
