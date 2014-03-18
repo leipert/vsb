a996365a04cca2448b71eba6133b154b873e8d03
meisenmann <1473362@googlemail.com>
2014-03-18 10:21:48 +0100

    Added spaces, semicolons and changed indentation.

--- app/js/controllers/subject.js
+++ app/js/controllers/subject.js
@@ -3,31 +3,28 @@
 /* Controllers */
 
 angular.module('GSB.controllers.subject', [])
-    .controller('SubjectController', ['$scope', function($scope) {
-        //Initial State of Subjects
-        $scope.subjects = [];
+.controller('SubjectController', ['$scope', function($scope) {
+  //Initial State of Subjects
+  $scope.subjects = [];
 
-        // Adds Subject with the provided URI
-        $scope.addSubject = function(uri){
-            
-			
-			$scope.subjects.push(
-                {
-                    alias: "",
-                    uri: uri,
-                    selectedProperties: [],
-                    availableProperties: []
-                }
-            );
-        }
+  // Adds Subject with the provided URI
+  $scope.addSubject = function(uri) {
+    $scope.subjects.push(
+      {
+        alias: "",
+        uri: uri,
+        selectedProperties: [],
+        availableProperties: []
+      }
+    );
+  }
 
-        //Adds first Subject
-        $scope.addSubject('http://a.de/Person');
-		
-		
-		//Removes the selected subject !!! FUNCTION IS NOT CALLED BY property.html DOES ANYBODY KNOW WHY?
-        $scope.removeSubject = function() {
-			alert('Test!');        //The subject must be removed here
-        }
+  //Adds first Subject
+  $scope.addSubject('http://a.de/Person');
 
-    }]);
+  //Removes the selected subject !!! FUNCTION IS NOT CALLED BY property.html DOES ANYBODY KNOW WHY?
+  $scope.removeSubject = function() {
+    alert('Test!');        //The subject must be removed here
+  }
+
+}]);  
