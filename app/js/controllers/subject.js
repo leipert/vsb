70aa1f3d9d0a5bf829b4acb9fa3985eb2091b813
Lukas Eipert <git@valedictorum.de>
2014-03-20 15:37:01 +0100

    Added "newSubjectEvent" Eventlistener for Subjectcontroller. Added function for creation of unique aliases.

--- app/js/controllers/subject.js
+++ app/js/controllers/subject.js
@@ -7,11 +7,12 @@ angular.module('GSB.controllers.subject', [])
   //Initial State of Subjects
   $scope.subjects = [];
 
-  // Adds Subject with the provided URI
-  $scope.addSubject = function(uri) {
+  // Adds Subject with the provided URI and Alias
+  $scope.addSubject = function(uri, alias) {
+    alias = $scope.createUniqueAlias(alias);
     $scope.subjects.push(
       {
-        alias: "",
+        alias: alias,
         uri: uri,
         selectedProperties: [],
         availableProperties: []
@@ -19,8 +20,34 @@ angular.module('GSB.controllers.subject', [])
     );
   }
 
+  //Checks whether an alias name is unique.
+  //If not it will provide us with an unique one.
+  $scope.createUniqueAlias=function(alias) {
+      var aliasUnique = true,
+          newAlias = alias,
+          key = null,
+          c = 1;
+      do{
+          for(key in $scope.subjects){
+              if($scope.subjects[key].alias === newAlias){
+                  aliasUnique = false;
+                  newAlias = alias + "_" + c;
+                  break;
+              }
+              aliasUnique = true;
+          }
+          c += 1;
+      }while(!aliasUnique);
+      return newAlias;
+  }
+
+  //Eventlistener. If this Event is called, a new Subject will be created.
+  $scope.$on('newSubjectEvent', function($event, subjectClass) {
+      $scope.addSubject(subjectClass.uri,subjectClass.alias);
+  });
+
   //Adds first Subject
-  $scope.addSubject('http://a.de/Person');
+  $scope.addSubject('/app/mockup/Person.json', "Mensch");
 
   //Removes the selected subject !!! FUNCTION IS NOT CALLED BY property.html DOES ANYBODY KNOW WHY?
   // yes we know: u must give the splice-function the instance of subject
