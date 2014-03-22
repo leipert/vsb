dd39c27b78303b3ff20856d06d405083ebeb4b57
meisenmann <1473362@googlemail.com>
2014-03-22 12:06:37 +0100

    Decided for on 'Choose Button' version
    Removed erroneous version of the button.
    Commented out modal version in index.html.
    Commented out ModalCtrl and ModalInstanceCtrl in app.js.

--- app/js/app.js
+++ app/js/app.js
@@ -50,62 +50,62 @@ var AddSubjectDropDownCtrl = function($scope, $http) {
 		};
 };
 
-var ModalCtrl = function($scope, $http, $modal, $log) {
-
-		$scope.items = []; // Is this the right place?
-
-		// Get Availabe Subject Classes from Server
-		$http.get('/app/mockup/classes.json').success(function(data) {
-				$scope.items = []; // This is a duplicate of the code 4 lines above. Any ideas which one is preferable?
-				var availClasses = data.results.bindings;
-				for (var key in availClasses) {
-						$scope.items.push(
-								{
-										alias: availClasses[key].alias.value,
-										uri: availClasses[key].class.value
-								}
-						);
-				}
-		});
-
-		$scope.open = function() {
-
-				// Create modalInstance
-				var modalInstance = $modal.open({
-						templateUrl: 'template/modal.html',
-						controller: ModalInstanceCtrl,
-						resolve: {
-								items: function() {
-										return $scope.items;
-								}
-						}
-				});
-
-				// User closed modal
-				modalInstance.result.then(function(selectedItem) {
-						$scope.selected = selectedItem;
-						$scope.$emit('newSubjectEvent', selectedItem);
-				}, function() {
-						$log.info('Modal dismissed at: ' + new Date());
-				});
-		};
-};
-
-var ModalInstanceCtrl = function($scope, $modalInstance, items) {
-
-		$scope.items = items;
-		$scope.selected = {
-				item: $scope.items[0]
-		};
-
-		// User clicked okay
-		$scope.ok = function() {
-				$modalInstance.close($scope.selected.item);
-		};
-
-		// USer clicked cancel
-		$scope.cancel = function() {
-				$modalInstance.dismiss('cancel');
-		};
-};
-
+//var ModalCtrl = function($scope, $http, $modal, $log) {
+//
+//		$scope.items = []; // Is this the right place?
+//
+//		// Get Availabe Subject Classes from Server
+//		$http.get('/app/mockup/classes.json').success(function(data) {
+//				$scope.items = []; // This is a duplicate of the code 4 lines above. Any ideas which one is preferable?
+//				var availClasses = data.results.bindings;
+//				for (var key in availClasses) {
+//						$scope.items.push(
+//								{
+//										alias: availClasses[key].alias.value,
+//										uri: availClasses[key].class.value
+//								}
+//						);
+//				}
+//		});
+//
+//		$scope.open = function() {
+//
+//				// Create modalInstance
+//				var modalInstance = $modal.open({
+//						templateUrl: 'template/modal.html',
+//						controller: ModalInstanceCtrl,
+//						resolve: {
+//								items: function() {
+//										return $scope.items;
+//								}
+//						}
+//				});
+//
+//				// User closed modal
+//				modalInstance.result.then(function(selectedItem) {
+//						$scope.selected = selectedItem;
+//						$scope.$emit('newSubjectEvent', selectedItem);
+//				}, function() {
+//						$log.info('Modal dismissed at: ' + new Date());
+//				});
+//		};
+//};
+//
+//var ModalInstanceCtrl = function($scope, $modalInstance, items) {
+//
+//		$scope.items = items;
+//		$scope.selected = {
+//				item: $scope.items[0]
+//		};
+//
+//		// User clicked okay
+//		$scope.ok = function() {
+//				$modalInstance.close($scope.selected.item);
+//		};
+//
+//		// USer clicked cancel
+//		$scope.cancel = function() {
+//				$modalInstance.dismiss('cancel');
+//		};
+//};
+//
