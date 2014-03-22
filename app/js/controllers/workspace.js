c641977c10115900773250966363858fc993e222
Lukas Eipert <git@valedictorum.de>
2014-03-22 09:19:21 +0100

    Implemented GSBL -> JSON. Closes #5

--- app/js/controllers/workspace.js
+++ app/js/controllers/workspace.js
@@ -6,8 +6,9 @@ angular.module('GSB.controllers.workspace', [])
 .controller('WorkspaceCtrl', ['$scope', function($scope) {
   //Initial State of Subjects
   $scope.subjects = [];
-  $scope.result = "In the near future the results will be displayed here.";
-  $scope.mainSubjectSelected;
+  $scope.translatedJSON = "In the near future the translated JSON will be here.";
+  $scope.translatedSPARQL = "In the near future the translated SPARQL will be here.";
+  $scope.mainSubjectSelected = null;
 
   // Adds Subject with the provided URI and Alias
   $scope.addSubject = function(uri, alias) {
@@ -20,7 +21,7 @@ angular.module('GSB.controllers.workspace', [])
         availableProperties: {}
       }
     );
-  }
+  };
 
   //Checks whether an alias name is unique.
   //If not it will provide us with an unique one.
@@ -41,7 +42,7 @@ angular.module('GSB.controllers.workspace', [])
           c += 1;
       }while(!aliasUnique);
       return newAlias;
-  }
+  };
 
   //Eventlistener. If this Event is called, a new Subject will be created.
   $scope.$on('newSubjectEvent', function($event, subjectClass) {
@@ -50,20 +51,40 @@ angular.module('GSB.controllers.workspace', [])
 
   //Adds first Subject
   $scope.addSubject('/app/mockup/Person.json', "Mensch");
+  $scope.addSubject('/app/mockup/Stadt.json', "Stadt");
 
   //Removes the selected subject !!! FUNCTION IS NOT CALLED BY property.html DOES ANYBODY KNOW WHY?
   // yes we know: u must give the splice-function the instance of subject
   $scope.removeSubject = function(subjectInst) {
     $scope.subjects.splice($scope.subjects.indexOf(subjectInst), 1);
-  }
+  };
 
-  $scope.translate = function(){
-      $scope.result = "";
-      for(var key in $scope.subjects){
-          var currSubject = angular.copy($scope.subjects[key]);
-          delete currSubject["availableProperties"];
-          $scope.result += JSON.stringify(currSubject) + "\n";
-      }
-  }
+  //Translate GSBL to JSON
+  $scope.translateGSBLToJSON = function(){
+    var json = {
+        START: {
+          type: "LIST_ALL",
+          "link": {
+            "direction" :"TO",
+            "linkPartner" : $scope.mainSubjectSelected.alias
+          }
+        },
+        SUBJECTS: []
+      },
+      allSubjects = angular.copy($scope.subjects)
+    allSubjects.map(function(currentSubject){
+      delete currentSubject["availableProperties"];
+      currentSubject.properties = c["selectedProperties"];
+      delete currentSubject["selectedProperties"];
+      return currentSubject;
+    });
+    json.SUBJECTS = allSubjects;
+    $scope.translatedJSON = JSON.stringify(json, null, 2);
+  };
+
+    //TODO: Translation from JSON to SPARQL ( Issue #6)
+    $scope.translateJSONtoSPARQL = function(){
+      $scope.translateGSBLToJSON();
+    };
 
 }]);  
