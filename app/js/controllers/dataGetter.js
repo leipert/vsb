e6d3c07571791a50a3233f2756713872f5c127e8
meisenmann <1473362@googlemail.com>
2014-03-27 20:20:01 +0100

    Tried factory as service.
    Both work.

--- app/js/controllers/dataGetter.js
+++ app/js/controllers/dataGetter.js
@@ -2,6 +2,30 @@
 
 
 angular.module('GSB.factories.dataGetter', []).
+  // service('classes', function($http, globalConfig) {
+  //   this.get = function(asc) {
+  //     console.log('classes.get called');
+  //     //  Get Availabe Subject Classes from Server
+  //     $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
+  //       var availClasses = data.results.bindings;
+  //       for (var key in availClasses) {
+  //         if(availClasses.hasOwnProperty(key)){
+  //           asc.push(
+  //             {
+  //               alias: availClasses[key].alias.value,
+  //               uri: availClasses[key].class.value,
+  //               comment: availClasses[key].comment ? availClasses[key].comment.value : 'No description available.'
+  //             }
+  //           );
+  //         }
+  //         // console.log(availClasses[key].alias.value);
+  //       }
+  //       // console.log(asc);
+  //     }).error(function() {
+  //       alert('error');
+  //     }); 
+  //   }
+  // });
   factory('classes', function($http, globalConfig) {
     var factory = {};
     factory.get = function(asc) {
@@ -20,6 +44,8 @@ angular.module('GSB.factories.dataGetter', []).
           }
           // console.log(availClasses[key].alias.value);
         }
+      }).error(function() {
+        alert('error');
       }); 
     }
     return factory;
