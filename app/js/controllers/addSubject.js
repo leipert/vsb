cb0808f3554ded3344399ec10037140820ccaec1
meisenmann <1473362@googlemail.com>
2014-03-27 14:15:03 +0100

    Moved dataGetter module to its own file.
    Not sure if app/controllers is the right place.

--- app/js/controllers/addSubject.js
+++ app/js/controllers/addSubject.js
@@ -1,29 +1,5 @@
 'use strict';
 
-
-angular.module('GSB.factories.dataGetter', [])
-  .factory('classes', function($http, globalConfig) {
-    var factory = {};
-    factory.get = function(asc) {
-      //  Get Availabe Subject Classes from Server
-      $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
-        var availClasses = data.results.bindings;
-        for (var key in availClasses) {
-          if(availClasses.hasOwnProperty(key)){
-            asc.push(
-              {
-                alias: availClasses[key].alias.value,
-                uri: availClasses[key].class.value
-              }
-            );
-          }
-          // console.log(availClasses[key].alias.value);
-        }
-      }); 
-    }
-    return factory;
-  });
-
 angular.module('GSB.controllers.addSubject', ['ngSanitize', 'ui.select'])
   .controller('AddSubjectCtrl', ['$scope', '$http', 'globalConfig', 'classes', function($scope, $http, globalConfig, classes) {
     //  List of available subject classes that can be added to the workspace.
---
+++ app/js/controllers/dataGetter.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+angular.module('GSB.factories.dataGetter', [])
+  .factory('classes', function($http, globalConfig) {
+    var factory = {};
+    factory.get = function(asc) {
+      //  Get Availabe Subject Classes from Server
+      $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
+        var availClasses = data.results.bindings;
+        for (var key in availClasses) {
+          if(availClasses.hasOwnProperty(key)){
+            asc.push(
+              {
+                alias: availClasses[key].alias.value,
+                uri: availClasses[key].class.value
+              }
+            );
+          }
+          // console.log(availClasses[key].alias.value);
+        }
+      }); 
+    }
+    return factory;
+  });
