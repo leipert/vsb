37180e5daa4a9405519f7d249c8c5c69fb24bf9b
meisenmann <1473362@googlemail.com>
2014-03-27 13:41:09 +0100

    Moved code to get classes from 'server' (json) to its own factory.

--- app/js/controllers/addSubject.js
+++ app/js/controllers/addSubject.js
@@ -1,37 +1,80 @@
 'use strict';
 
 angular.module('GSB.controllers.addSubject', ['ngSanitize', 'ui.select'])
-.controller('AddSubjectCtrl', ['$scope', '$http', 'globalConfig', function($scope, $http, globalConfig) {
-//	 List of available subject classes that can be added to the workspace.
-  $scope.availableSubjectClasses = [];
-
-  //  Subject selected to be added to the workspace.
-  $scope.selectedSubjectClass = undefined;
-
-  //  Get Availabe Subject Classes from Server
-  $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
-    var availClasses = data.results.bindings;
-    for (var key in availClasses) {
-      if(availClasses.hasOwnProperty(key)){
-        $scope.availableSubjectClasses.push(
-          {
-            alias: availClasses[key].alias.value,
-            uri: availClasses[key].class.value
+  .factory('test', function($http, globalConfig) {
+    var factory = {};
+    factory.getClasses = function(asc) { // version 1
+      //  Get Availabe Subject Classes from Server
+      $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
+        var availClasses = data.results.bindings;
+        for (var key in availClasses) {
+          if(availClasses.hasOwnProperty(key)){
+            asc.push(
+              {
+                alias: availClasses[key].alias.value,
+                uri: availClasses[key].class.value
+              }
+            );
           }
-        );
-      }
-      // console.log(availClasses[key].alias.value);
+          // console.log(availClasses[key].alias.value);
+        }
+      }); 
     }
-  }); 
-		
-		
+    // factory.getClasses = function() { // version 2
+    //   var asc = [];
+    //   //  Get Availabe Subject Classes from Server
+    //   $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
+    //     var availClasses = data.results.bindings;
+    //     for (var key in availClasses) {
+    //       if(availClasses.hasOwnProperty(key)){
+    //         asc.push(
+    //           {
+    //             alias: availClasses[key].alias.value,
+    //             uri: availClasses[key].class.value
+    //           }
+    //         );
+    //       }
+    //       // console.log(availClasses[key].alias.value);
+    //     }
+    //   }); 
+    //   return asc;
+    // }
+    return factory;
+  })
+  .controller('AddSubjectCtrl', ['$scope', '$http', 'globalConfig', 'test', function($scope, $http, globalConfig, test) {
+    //  List of available subject classes that can be added to the workspace.
+    $scope.availableSubjectClasses = [];
+
+    //  Subject selected to be added to the workspace.
+    $scope.selectedSubjectClass = undefined;
+
+    // //  Get Availabe Subject Classes from Server
+    // $http.get(globalConfig.baseURL + 'mockup/classes.json').success(function(data) {
+    //   var availClasses = data.results.bindings;
+    //   for (var key in availClasses) {
+    //     if(availClasses.hasOwnProperty(key)){
+    //       $scope.availableSubjectClasses.push(
+    //         {
+    //           alias: availClasses[key].alias.value,
+    //           uri: availClasses[key].class.value
+    //         }
+    //       );
+    //     }
+    //     // console.log(availClasses[key].alias.value);
+    //   }
+    // }); 
+    
+    
+		test.getClasses($scope.availableSubjectClasses);       // version 1
+		//$scope.availableSubjectClasses = test.getClasses();  // version 2
+
 		$scope.uiAddSubject = function() {
-				console.log($scope.selectedSubjectClass);
-    if($scope.selectedSubjectClass) { // If the selected option is undefined no subject will be added.
-      $scope.$emit('newSubjectEvent', $scope.selectedSubjectClass);
-						  $scope.selectedSubjectClass = undefined;
-    }
-  };
-}]);
+			console.log($scope.selectedSubjectClass);
+      if($scope.selectedSubjectClass) { // If the selected option is undefined no subject will be added.
+        $scope.$emit('newSubjectEvent', $scope.selectedSubjectClass);
+				$scope.selectedSubjectClass = undefined;
+      }
+    };
+  }]);
 
 
