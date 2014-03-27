fce0dcb6f2604865f80e0cfc12118c9e3cd54596
meisenmann <1473362@googlemail.com>
2014-03-27 15:34:36 +0100

    Indentation fix.

--- app/js/controllers/addSubject.js
+++ app/js/controllers/addSubject.js
@@ -1,20 +1,20 @@
 'use strict';
 
-angular.module('GSB.controllers.addSubject', ['ngSanitize', 'ui.select'])
-  .controller('AddSubjectCtrl', ['$scope', '$http', 'globalConfig', 'classes', function($scope, $http, globalConfig, classes) {
+angular.module('GSB.controllers.addSubject', ['ngSanitize', 'ui.select']).
+  controller('AddSubjectCtrl', ['$scope', '$http', 'globalConfig', 'classes', function($scope, $http, globalConfig, classes) {
     //  List of available subject classes that can be added to the workspace.
     $scope.availableSubjectClasses = [];
 
     //  Subject selected to be added to the workspace.
     $scope.selectedSubjectClass = undefined;
     
-		classes.get($scope.availableSubjectClasses);       // version 1
-	
-	$scope.uiAddSubject = function() {
-			console.log($scope.selectedSubjectClass);
+    classes.get($scope.availableSubjectClasses);       // version 1
+    
+    $scope.uiAddSubject = function() {
+      console.log($scope.selectedSubjectClass);
       if($scope.selectedSubjectClass) { // If the selected option is undefined no subject will be added.
         $scope.$emit('newSubjectEvent', $scope.selectedSubjectClass);
-				$scope.selectedSubjectClass = undefined;
+	      $scope.selectedSubjectClass = undefined;
       }
     };
   }]);
