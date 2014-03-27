92f3e22885433739590e28310212c5fab03805eb
meisenmann <1473362@googlemail.com>
2014-03-27 20:17:59 +0100

    Removed unnecessary stuff.

--- app/js/controllers/addSubject.js
+++ app/js/controllers/addSubject.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('GSB.controllers.addSubject', ['ngSanitize', 'ui.select']).
-  controller('AddSubjectCtrl', ['$scope', '$http', 'globalConfig', 'classes', function($scope, $http, globalConfig, classes) {
+  controller('AddSubjectCtrl', ['$scope', 'classes', function($scope, classes) {
     //  List of available subject classes that can be added to the workspace.
     $scope.availableSubjectClasses = [];
 
     //  Subject selected to be added to the workspace.
     $scope.selectedSubjectClass = undefined;
     
-    classes.get($scope.availableSubjectClasses);       // version 1
+    classes.get($scope.availableSubjectClasses);
     
     $scope.uiAddSubject = function() {
       console.log($scope.selectedSubjectClass);
