5b9e3cf60fc388fc3143c077c1c45e56fe02ec92
Lukas Eipert <git@valedictorum.de>
2014-04-05 13:49:14 +0200

    Refactored Workspace Controller.

--- app/js/controllers/workspace.js
+++ app/js/controllers/workspace.js
@@ -1,19 +1,27 @@
 'use strict';
 
-/* Controllers */
+/**
+ * WorkspaceCtrl
+ * Controller which holds all the subjects.
+ * TODO: Better definition what this controller actually does
+ */
 
 angular.module('GSB.controllers.workspace', ['GSB.config', 'GSB.services.availableClasses'])
   .controller('WorkspaceCtrl', ['$scope', '$log', 'AvailableClassesService', 'globalConfig', function ($scope, $log, AvailableClassesService, globalConfig) {
-    //Initial State of Subjects
-    $scope.subjects = [];
-    $scope.highlightedSubject = null;
-    $scope.translatedJSON = "In the near future the translated JSON will be here.";
-    $scope.translatedSPARQL = "In the near future the translated SPARQL will be here.";
-    $scope.mainSubjectSelected = null;
-    $scope.expertView = false;
-    
-    // Adds Subject with the provided URI and Alias
+
+    //** FUNCTIONS START **//
+
+    /**
+     * a function which adds a new subject with an uri, alias and comment
+     *
+     * TODO: Handle empty alias (maybe in createUniqueAlias) & empty comment
+     *
+     * @param uri
+     * @param alias
+     * @param comment
+     */
     $scope.addSubject = function (uri, alias, comment) {
+      $log.info('Subject added');
       alias = $scope.createUniqueAlias(alias);
       $scope.subjects.push(
         {
@@ -27,78 +35,80 @@ angular.module('GSB.controllers.workspace', ['GSB.config', 'GSB.services.availab
           showAdditionalFields: false
         }
       );
+      //If there is only one subject, it will be automatically the selected by the startPoint.
+      //TODO: Move to separate $watch ???
       if ($scope.subjects.length === 1) {
         $scope.mainSubjectSelected = $scope.subjects[0];
       }
     };
 
-    //Checks whether an alias name is unique.
-    //If not it will provide us with an unique one.
+    /**
+     * returns a unique alias for a given alias
+     *
+     * TODO: Handle empty alias
+     *
+     * @param alias
+     * @returns {*}
+     */
     $scope.createUniqueAlias = function (alias) {
       var aliasUnique = true,
-      newAlias = alias,
-      key = null,
-      c = 1;
+        newAlias = alias,
+        key = null,
+        c = 1;
       do {
         for (key in $scope.subjects) {
-          if ($scope.subjects[key].alias === newAlias) {
-            aliasUnique = false;
-            newAlias = alias + "_" + c;
-            break;
+          if ($scope.subjects.hasOwnProperty(key)) {
+            if ($scope.subjects[key].alias === newAlias) {
+              aliasUnique = false;
+              newAlias = alias + "_" + c;
+              break;
+            }
+            aliasUnique = true;
           }
-          aliasUnique = true;
+          c += 1;
         }
-        c += 1;
       } while (!aliasUnique);
       return newAlias;
     };
 
-    //Eventlistener. If this Event is called, a new Subject will be created.
-    $scope.$on('newSubjectEvent', function ($event, subjectClass) {
-	    $scope.addSubject(subjectClass.uri, subjectClass.alias, subjectClass.comment);
-    });
-
-
-    //Adds first Subject
-     //  List of available subject classes that can be added to the workspace.
-    $scope.availableSubjectClasses = [];
-    AvailableClassesService.get($scope.availableSubjectClasses);
-    console.log($scope.availableSubjectClasses);
-	  
-// Does not work, $scope.availableSubjectClasses stille empty at this point.
-// Does sombody know why?
-//    $scope.addSubject($scope.asc[0].uri, $scope.asc[0].alias, $scope.asc[0].comment);
-//    $scope.addSubject($scope.asc[1].uri, $scope.asc[1].alias, $scope.asc[1].comment);
-    $scope.addSubject('mockup/Person.json', "Mensch", "Ein Individuum der Spezies Mensch.");
-    $scope.addSubject('mockup/Stadt.json', "Stadt", "Eine Siedlung, größer als ein Dorf.");
-
-    //Removes the selected subject !!! FUNCTION IS NOT CALLED BY property.html DOES ANYBODY KNOW WHY?
-    // yes we know: u must give the splice-function the instance of subject
+    /**
+     * Removes a given subject from the Workspace
+     * @param subjectInst
+     */
     $scope.removeSubject = function (subjectInst) {
       $scope.subjects.splice($scope.subjects.indexOf(subjectInst), 1);
+      //If there is only one subject, it will be automatically the selected by the startPoint.
+      //TODO: Move to separate $watch ???
       if ($scope.subjects.length === 1) {
         $scope.mainSubjectSelected = $scope.subjects[0];
       }
     };
 
-    //Translate GSBL to JSON
+
+    /**
+     * translates the GSBL to JSON
+     * TODO: Make it nicer
+     * TODO: Move it to a own service|factory ???
+     * @namespace currentProperty.link.linkPartner
+     */
     $scope.translateGSBLToJSON = function () {
+      $log.info('Translate GSBL to JSON');
       if ($scope.mainSubjectSelected == null) {
         $log.error("Main Subject not connected");
         $scope.translatedJSON = null;
         return;
       }
       var json = {
-        START: {
-          type: "LIST_ALL",
-          "link": {
-            "direction": "TO",
-            "linkPartner": $scope.mainSubjectSelected.alias
-          }
+          START: {
+            type: "LIST_ALL",
+            "link": {
+              "direction": "TO",
+              "linkPartner": $scope.mainSubjectSelected.alias
+            }
+          },
+          SUBJECTS: []
         },
-        SUBJECTS: []
-      },
-      allSubjects = angular.copy($scope.subjects);
+        allSubjects = angular.copy($scope.subjects);
       allSubjects.map(function (currentSubject) {
         delete currentSubject["availableProperties"];
         currentSubject.properties = currentSubject["selectedProperties"].map(function (currentProperty) {
@@ -118,8 +128,13 @@ angular.module('GSB.controllers.workspace', ['GSB.config', 'GSB.services.availab
       $scope.translatedJSON = JSON.stringify(json, null, 2);
     };
 
-    //TODO: Translation from JSON to SPARQL ( Issue #6)
+    /**
+     * translates the JSON to SPARQL
+     * TODO: Make it nicer
+     * TODO: Move it to a own service|factory ???
+     */
     $scope.translateJSONtoSPARQL = function () {
+      $log.info('Translate JSON to SPARQL');
       $scope.translateGSBLToJSON();
       if ($scope.translatedJSON == null) {
         $log.error("JSON is not valid");
@@ -128,18 +143,62 @@ angular.module('GSB.controllers.workspace', ['GSB.config', 'GSB.services.availab
       $scope.translatedSPARQL = translateAllFromString($scope.translatedJSON);
     };
 
-    //Reset workspace function
+    /**
+     * Initializes the Workspace
+     */
     $scope.initializeWorkspace = function () {
+      $log.info('Initialize Workspace');
+      $scope.availableSubjectClasses = [];
       $scope.subjects = [];
+      $scope.highlightedSubject = null;
+      $scope.translatedJSON = "In the near future the translated JSON will be here.";
+      $scope.translatedSPARQL = "In the near future the translated SPARQL will be here.";
+      $scope.mainSubjectSelected = null;
+      $scope.expertView = false;
+      AvailableClassesService.get($scope.availableSubjectClasses);
     };
 
-    $scope.toggleSubjectView = function(subjectInst){
+    /**
+     * Change the View of the given Subject
+     * TODO: Maybe move it to own controller?
+     * @param subjectInst
+     */
+    $scope.toggleSubjectView = function (subjectInst) {
       subjectInst.view = !subjectInst.view;
-    }
+    };
 
+    /**
+     * Open the SPARQL Query in a new dbpedia tab
+     * TODO: implement our own Result view ;)
+     */
     $scope.openInNewTab = function () {
-        var win=window.open(globalConfig.queryURL + encodeURIComponent($scope.translatedSPARQL), '_blank');
-        win.focus();
-    }
+      var win = window.open(globalConfig.queryURL + encodeURIComponent($scope.translatedSPARQL), '_blank');
+      win.focus();
+    };
+
+    //** FUNCTIONS END **//
+
+    //Initialize Workspace and add two subjects to it (for easier testing).
+    $scope.initializeWorkspace();
+    $scope.addSubject('mockup/Person.json', "Mensch", "Ein Individuum der Spezies Mensch.");
+    $scope.addSubject('mockup/Stadt.json', "Stadt", "Eine Siedlung, größer als ein Dorf.");
+
+    /**
+     * New Event. If this event is broadcast by another service or controller,
+     * a new subject will be added
+     *
+     * @param $event The $event which has been triggered
+     * @param subjectClass
+     * @namespace subjectClass.uri
+     * @namespace subjectClass.alias
+     * @namespace subjectClass.comment
+     */
+    $scope.$on('newSubjectEvent', function ($event, subjectClass) {
+      if(subjectClass.hasOwnProperty('uri')) {
+        $scope.addSubject(subjectClass.uri, subjectClass.alias, subjectClass.comment);
+      }else{
+        $log.error('newSubjectEvent - no URI provided');
+      }
+    });
 
   }]);
