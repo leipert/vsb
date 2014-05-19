a2e4a4b29dd232070bc8bddcbae920af3a81ed52
Lukas Eipert <git@valedictorum.de>
2014-05-19 14:02:14 +0200

    Added date comparison.

--- app/js/controllers/propertyType/date.js
+++ app/js/controllers/propertyType/date.js
@@ -5,41 +5,60 @@
  * Controller for a single property.
  */
 
-angular.module('GSB.controllers.propertyType.date', ['GSB.config'])
+angular.module('GSB.controllers.propertyType.date', ['GSB.config','angular-momentjs'])
     //Inject $scope, $http, $log and globalConfig (see @ js/config.js) into controller
     .controller('DatePropertyCtrl', ['$scope', '$http', '$log', 'globalConfig', function($scope, $http, $log, globalConfig) {
 
-        $scope.allowedDateComparisons = [
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+
+    $scope.allowedDateComparisons = [
             {
                 label: "equals",
-                f : '(str(%after_arithmetic%)="%input%")'
+                f : 'xsd:dateTime(%after_arithmetic%) >= "%input_start_of_day%"^^xsd:dateTime' +
+                '&& xsd:dateTime(%after_arithmetic%) <= "%input_end_of_day%"^^xsd:dateTime'
             },
             {
                 label: "equals not",
-                f : '(str(%after_arithmetic%)!="%input%")'
+              f : 'xsd:dateTime(%after_arithmetic%) < "%input_start_of_day%"^^xsd:dateTime' +
+              '|| xsd:dateTime(%after_arithmetic%) > "%input_end_of_day%"^^xsd:dateTime'
             },
             {
                 label: "after",
-                f : 'regex(%after_arithmetic%, "^%input%", "i")'
+              f : 'xsd:dateTime(%after_arithmetic%) > "%input_end_of_day%"^^xsd:dateTime'
             },
             {
                 label: "before",
-                f : 'regex(%after_arithmetic%, "%input%$", "i")'
+              f : 'xsd:dateTime(%after_arithmetic%) < "%input_start_of_day%"^^xsd:dateTime'
             }
         ];
 
 
         $scope.dateComparison = null;
 
-        $scope.$watch('dateComparison',function (newValue){});
+        $scope.$watch('dateComparison',function (newValue){
+          renderDate();
+        });
 
         $scope.comparisonInput = "";
 
-        $scope.$watch('comparisonInput',function (newValue){});
+        $scope.$watch('comparisonInput',function (newValue){
+          renderDate();
+        });
 
-        $scope.comparisonRegexFlags = "j";
-
-
-        $scope.selectedLanguage = null;
+        function renderDate(){
+          if($scope.dateComparison == null ){
+            $scope.propertyInst.compare = null;
+          }
+          $scope.propertyInst.compare =
+            $scope.dateComparison.f
+              .replace(/%input_start_of_day%/g,moment($scope.comparisonInput).utc().hour(0).minute(0).second(0).format())
+              .replace(/%input_end_of_day%/g,moment($scope.comparisonInput).utc().hour(23).minute(59).second(59).format())
+        }
 
     }]);
