71606677ef3ead90c0f17011ed3015d86b3d211f
Lukas Eipert <git@valedictorum.de>
2014-06-03 13:25:04 +0200

    Removed unnecessary filter as angular can do it natively.

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,24 +1,5 @@
 'use strict';
 angular.module('GSB.filters', [])
-  .filter('filterArrayOfObjectsByKeyWithArray', function() {
-    /**
-     * Filters an array of Objects based on a given Key and allowed values for that key
-     * @param arrayOfObjects
-     * @param filterKey
-     * @param allowedKeyValues
-     */
-    return function(arrayOfObjects, filterKey, allowedKeyValues) {
-      if(allowedKeyValues.length == 0){
-        return arrayOfObjects;
-      }
-        return arrayOfObjects.filter(
-        function(currentObject){
-          return this.allowedKeyValues.indexOf(currentObject[this.filterKey]) != -1;
-        },
-        {allowedKeyValues: allowedKeyValues, filterKey: filterKey}
-      );
-    };
-  })
   .filter('aggregatePropertyFilter',function(){
     return function(arrayOfObjects, filter){
       if(filter === null || filter === undefined || !filter.hasOwnProperty("restrictTo") || filter.restrictTo === null){
